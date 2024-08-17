48ca61f0bedefadd648bad1d5305d4297e0f4910
Lasse <jugj@users.noreply.github.com>
2024-08-17 12:37:25 +0200

    Änderungen von Lasse um 12:37:25

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private static float speed = 1f;
+    private bool colliding;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 current = transform.position;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) transform.position += Vector3.left * speed;
+        if (Input.GetKeyDown(KeyCode.RightArrow)) transform.position += Vector3.right * speed;
+        if (Input.GetKeyDown(KeyCode.UpArrow)) transform.position += Vector3.up * speed;
+        if (Input.GetKeyDown(KeyCode.DownArrow)) transform.position += Vector3.down * speed;
+
+        if (colliding)
+        {
+            transform.position = current;
+            colliding = false;
+        }
+
+        Debug.Log("hi");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("colliding");
+        colliding = true;
     }
 }
