c1e9363f14d48137b55ce48c25bfd1d1d0680596
Aaron <jugj@users.noreply.github.com>
2024-08-17 12:13:27 +0200

    Änderungen von Aaron um 12:13:27

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,26 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5;
-    private Rigidbody2D rb;
-    public float jumpForce = 5;
-
+    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float direction = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * speed * direction * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+        
     }
 }
-
