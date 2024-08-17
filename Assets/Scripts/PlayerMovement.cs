a82bff08b2f8756294a4c84d5e90252eeb174ca6
Aaron <jugj@users.noreply.github.com>
2024-08-17 16:03:44 +0200

    Änderungen von Aaron um 16:03:44

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,46 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed = 10f;  // Erhöht, da AddForce typischerweise größere Werte benötigt
     private List<GameObject> list = new List<GameObject>();
     public Texture2D texture;
-    public Rigidbody2D rb; //establishes a variable for this code, "rb" for "RigidBody"
+    public Rigidbody2D rb;
 
-    // Start is called before the first frame update
     void Start()
     {
-//       //rb = gameObject.GetComponent<Rigidbody2D>();
-//       for (int i = 0; i < 20; i++)
-//       {
-//           GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Quad);
-//           list.Add(obj);
-//           Renderer renderer = obj.GetComponent<Renderer>();
-//           renderer.material.mainTexture = texture;
-//           obj.transform.position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), 0);
-//       }
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-//        Vector3 current = transform.position;
+        Vector2 movement = Vector2.zero;
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
-            transform.Translate(Vector2.left * speed, Space.World);
-        };
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
-            transform.Translate(Vector2.right * speed , Space.World);
-        };
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
-            transform.Translate(Vector2.up * speed , Space.World);
-        };
-        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
-            transform.Translate(Vector2.down * speed, Space.World);
-        };
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            movement += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            movement += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            movement += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            movement += Vector2.down;
+        }
 
-        //GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
-        //foreach (GameObject go in allObjects)
-        //    if (go.activeInHierarchy)
-        //        if (false && go.transform.position != transform.position && Vector3.Distance(go.transform.position, transform.position) < 1) transform.position = current;
+        rb.AddForce(movement.normalized * speed);
     }
 }
