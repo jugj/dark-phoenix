85c457df1285201be95a66a3adc1bb8a2c0909bb
Lasse <jugj@users.noreply.github.com>
2024-08-17 13:08:06 +0200

    Änderungen von Lasse um 13:08:06

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,36 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private static float speed = 1f;
-    private bool colliding;
+    private final List<GameObject> list = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            list.Add(obj);
+            Renderer renderer = obj.GetComponent<Renderer>();
+            renderer.material.mainTexture = texture;
+            obj.transform.position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 current = transform.position;
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) transform.position += Vector3.left * speed;
-        if (Input.GetKeyDown(KeyCode.RightArrow)) transform.position += Vector3.right * speed;
-        if (Input.GetKeyDown(KeyCode.UpArrow)) transform.position += Vector3.up * speed;
-        if (Input.GetKeyDown(KeyCode.DownArrow)) transform.position += Vector3.down * speed;
-
-        if (colliding)
-        {
-            transform.position = current;
-            colliding = false;
-        }
-
-        Debug.Log("hi");
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("colliding");
-        colliding = true;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) transform.Translate(Vector3.left * speed);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) transform.Translate(Vector3.right * speed);
+        if (Input.GetKeyDown(KeyCode.UpArrow)) transform.Translate(Vector3.up * speed);
+        if (Input.GetKeyDown(KeyCode.DownArrow)) transform.Translate(Vector3.down * speed);
     }
 }
