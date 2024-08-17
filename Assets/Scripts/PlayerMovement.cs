703d92348351fac505e2e96ac0d4769ef41b22c0
Lasse <jugj@users.noreply.github.com>
2024-08-17 14:15:26 +0200

    Änderungen von Lasse um 14:15:26

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private static float speed = 1f;
-    private final List<GameObject> list = new List<GameObject>();
+    private List<GameObject> list = new List<GameObject>();
+    public Texture2D texture;
+    public Rigidbody2D rb; //establishes a variable for this code, "rb" for "RigidBody"
 
     // Start is called before the first frame update
     void Start()
     {
+        //rb = gameObject.GetComponent<Rigidbody2D>();
         for (int i = 0; i < 20; i++)
         {
             GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Quad);
@@ -23,9 +26,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) transform.Translate(Vector3.left * speed);
-        if (Input.GetKeyDown(KeyCode.RightArrow)) transform.Translate(Vector3.right * speed);
-        if (Input.GetKeyDown(KeyCode.UpArrow)) transform.Translate(Vector3.up * speed);
-        if (Input.GetKeyDown(KeyCode.DownArrow)) transform.Translate(Vector3.down * speed);
+        Vector3 current = transform.position;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) transform.Translate(Vector3.left * speed);
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) transform.position += (Vector3.right * speed);
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) transform.position += (Vector3.up * speed);
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) transform.position += (Vector3.down * speed);
+
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        foreach (GameObject go in allObjects)
+            if (go.activeInHierarchy)
+                if (false && go.transform.position != transform.position && Vector3.Distance(go.transform.position, transform.position) < 1) transform.position = current;
     }
 }
